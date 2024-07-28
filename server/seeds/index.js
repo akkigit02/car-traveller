65e1902ca65d4ec0501fbc41026e04eea768f370
Galok Sarkar <galoksarkar@Galoks-MacBook-Air.local>
2024-07-28 22:16:51 +0530

    design

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,10 +1,13 @@
+    require('dotenv').config();
+    require('../configs/database.config');
+
+
 const collections = [
     'user'
 ];
 
 (async () => {
-    require('dotenv').config();
-    require('../configs/database.config');
+    
     console.log("Seed start...")
     for(let collection of collections) {
         let  data = require(`./${collection}.seed.json`);
