327f91fcfa77dc1d1d9bfe73e98e2076e8eac365
parthm_12 <pratham.das@agamitechnologies.in>
2024-07-31 23:39:05 +0530

    resolve issue

--- client/app/src/routes/protected/AdminRoute.js
+++ client/app/src/routes/protected/AdminRoute.js
@@ -2,16 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import AdminDashboard from "../../components/admin/Dashboard";
 import React from 'react'
 import SideNavBar from "../../components/SideNavBar";
+import VehiclePricing from "../../components/admin/VehiclePricing";
 
 function AdminRoute() {
   return (
     <>
-    <main>
-      <SideNavBar />
-      {/* <Routes>
-        <Route path={`/`} Component={AdminDashboard} name="ADMIN_DASHBOARD" exact />
-        <Route path={`/dashboard`} Component={AdminDashboard} name="ADMIN_DASHBOARD" exact />
-      </Routes> */}
+      <main>
+        <SideNavBar />
+        <Routes>
+          <Route path={`/`} Component={AdminDashboard}  exact />
+          <Route path={`/dashboard`} Component={AdminDashboard}  exact />
+          <Route path={`/vehicle-pricing`} Component={VehiclePricing}  exact />
+        </Routes>
       </main>
     </>
   )
