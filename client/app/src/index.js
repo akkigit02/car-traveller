5340ae044e9d2cd86322195585dc536f9dd3d190
avinash <avinashrathor8896@gmail.com>
2024-08-06 21:28:14 +0530

    css file

--- client/app/src/index.js
+++ client/app/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './configs/axios.config.js'
-import './css/style.css';
+import './assets/css/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 console.log(process.env.REACT_APP_SERVER_BASE_URL)
 const root = ReactDOM.createRoot(document.getElementById('root'));
