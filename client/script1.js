4224bf39386f70cad7cbd2b50d8f7b95f5776eec
avinash <avinashrathor8896@gmail.com>
2024-08-09 22:16:57 +0530

    date time picker

--- client/script1.js
+++ client/script1.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
     submitSuttom.addEventListener('click', () => {
 
         //  error handle 
-        console.log(query)
         const jsonString = JSON.stringify(query);
         const encodedString = btoa(jsonString);
         console.log(encodedString);
@@ -84,4 +83,47 @@ document.addEventListener('DOMContentLoaded', () => {
     to.addEventListener('input', (event) => { filterFunction('to') })
     to.addEventListener('focus', (event) => { setSuggestionVisible('to', true) })
     // to.addEventListener('blur', (event) => { setSuggestionVisible('to', false) })
+
+ const getTimeForDropdown = () => {
+     const timeSelect = document.getElementById('timeSelect');
+     const date = new Date();
+     const endDate = new Date();
+     endDate.setHours(23, 45, 0, 0); // 11:45 PM
+     const optionsInterval = 15; // interval in minutes
+
+     const datepickerElement = document.getElementById('datepicker');
+     const inputElement = datepickerElement.querySelector('input');
+
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const year = date.getFullYear();
+
+        const formattedDate = day + '-' + month + '-' + year;
+        inputElement.value = formattedDate;
+
+     // Round the current minutes up to the nearest 30-minute interval
+     const now = new Date();
+
+     while (date <= endDate) {
+         const hours = date.getHours();
+         const minutes = Math.ceil(date.getMinutes()/optionsInterval)*optionsInterval;
+         const ampm = hours >= 12 ? 'PM' : 'AM';
+         const displayHours = hours % 12 === 0 ? 12 : hours % 12;
+         const displayMinutes = minutes < 10 ? `0${minutes}` : minutes;
+         const displayTime = `${displayHours}:${displayMinutes} ${ampm}`;
+         
+         const isoTime = date.toISOString();
+         
+         const option = document.createElement('option');
+         option.value = isoTime;
+         option.textContent = displayTime;
+         if (date.getHours() === now.getHours() && date.getMinutes() === now.getMinutes()) {
+            option.selected = true;
+        }
+         timeSelect.appendChild(option);
+         date.setMinutes(date.getMinutes() + optionsInterval);
+     }
+ }
+
+ getTimeForDropdown()
 })
