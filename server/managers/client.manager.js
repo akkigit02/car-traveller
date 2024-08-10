11f516da60a6d5cf5e109a2597a77bc9a5fb27ea
Harsh Raj Yadav <harshrajyadav82@gmail.com>
2024-08-10 14:32:32 +0530

    booking api

--- server/managers/client.manager.js
+++ server/managers/client.manager.js
@@ -1,5 +1,5 @@
 const CitiesModel = require('../models/cities.model');
-
+const RideModel = require('../models/ride.model')
 
 const getCities = async (req, res) => {
     try {
@@ -31,7 +31,75 @@ const getCars = async (req, res) => {
         return res.status(500).send({ message: 'Something went wrong' })
     }
 }
+const addBooking = async (req, res) =>{
+    try {
+        let bookingDetails = req.body
+        bookingDetails.status = "completed"
+        const booking = await RideModel.create(bookingDetails)
+        return res.status(200).send({booking,message: 'Ride has been Booked'})     
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({ message: 'Something went wrong' })
+    }
+}
+const getBooking = async(req, res) => {
+    try {
+        const booking = await RideModel.find().lean();
+        res.status(200).send({booking})
+    } catch (error) {
+        console.log(error)
+    }
+}
+const getBookingByPasssengerId = async(req, res) => {
+    try {
+        const passengerId = req.params.id
+        const booking = await RideModel.find({passengerId}).lean();
+        res.status(200).send({booking})
+    } catch (error) {
+        console.log(error)
+    }
+}
+const cancelBooking = async (req, res) => {
+    try {
+        const rideId = req.params.id
+        const ride = await RideModel.findOne({_id:rideId})
+
+        if (!ride) {
+            return res.status(404).send({ message: 'Ride not found' });
+        }
+
+        const { pickupDate, pickupTime, status } = ride;
+        
+        if (status === "cancelled") {
+            return res.status(500 ).send({ message: 'Ride is Already Cancelled' });
+        }
+
+        const pickupDateTime = new Date(`${pickupDate}T${pickupTime}`);
+
+        const currentTime = new Date();
+
+        const timeDifference = (pickupDateTime - currentTime) / (1000 * 60);
+
+        if (timeDifference < 90) {
+            return res.status(400).send({ message: 'Ride can only be cancelled before 90 minutes of the pickup time' });
+        }
+
+        await RideModel.findOneAndUpdate({ _id: rideId },{ status: "cancelled" });
+        return res.status(200).send({ message: 'Ride cancelled successfully' });
+
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({ message: 'Something went wrong' }) 
+    }
+}
+
 module.exports = {
     getCities,
-    getCars
+    getCars,
+    addBooking,
+    getBooking,
+    getBookingByPasssengerId,
+    cancelBooking
+
 }
--- server/routes/client.route.js
+++ server/routes/client.route.js
@@ -3,5 +3,9 @@ const ClientManager = require('../managers/client.manager')
 
 router.get('/cities', ClientManager.getCities)
 router.get('/car-list', ClientManager.getCars)
+router.post('/booking',ClientManager.addBooking)
+router.get('/booking',ClientManager.getBooking)
+router.get('/passanger-booking/:id',ClientManager.getBookingByPasssengerId)
+router.put('/cancel-booking/:id',ClientManager.cancelBooking)
 
 module.exports = router
