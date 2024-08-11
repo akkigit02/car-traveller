9cd16adfc150460012ef1e95044a0c7fa5f68c21
avinash <avinashrathor8896@gmail.com>
2024-08-11 18:40:21 +0530

    issue

--- client/app/src/configs/axios.config.js
+++ client/app/src/configs/axios.config.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
-console.log(333333,222)
+
 const { REACT_APP_SERVER_BASE_URL } = process.env
-console.log(333333,REACT_APP_SERVER_BASE_URL)
+
 Axios.defaults.baseURL = REACT_APP_SERVER_BASE_URL;
 Axios.defaults.headers.post['Content-Type'] = 'application/json';
 
