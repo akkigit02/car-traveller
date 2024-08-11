a36107c8edbdfba4cbf9a485611e15aecae763bb
parthm_12 <pratham.das@agamitechnologies.in>
2024-08-11 16:10:39 +0530

    add payment file

--- client/app/src/components/Signup.js
+++ client/app/src/components/Signup.js
@@ -5,55 +5,55 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import TopNavBar from './TopNavBar';
 function Signup() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const { query } = useParams();
-    const [bookingDetails, setBookingDetails] = useState()
-    const [isOtpSent, setIsOtpSent] = useState(false)
-    const [sessionId, setSessionId] = useState()
-    const [otp, setOtp] = useState()
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { query } = useParams();
+  const [bookingDetails, setBookingDetails] = useState()
+  const [isOtpSent, setIsOtpSent] = useState(false)
+  const [sessionId, setSessionId] = useState()
+  const [otp, setOtp] = useState()
 
 
-    const verifyOtp = async () => {
-        try {
-            const { data } = await axios({
-                url: '/api/auth/verify-otp',
-                method: 'POST',
-                data: {
-                    otp, sessionId
-                }
-            })
-            console.log(data)
-        } catch (error) {
-            console.log(error.response.data)
+  const verifyOtp = async () => {
+    try {
+      const { data } = await axios({
+        url: '/api/auth/verify-otp',
+        method: 'POST',
+        data: {
+          otp, sessionId
         }
+      })
+    } catch (error) {
+      console.log(error.response.data)
     }
-  
+  }
 
-    const sendOtp = async (formData) => {
-        try {
-            const { data } = await axios({
-                url: '/api/auth/send-otp',
-                method: 'POST',
-                data: { userDetails: formData, bookingDetails }
-            })
-            if (data.status === 'TWO_STEP_AUTHENTICATION') {
-                setIsOtpSent(true)
-                setSessionId(data.sessionId)
-            }
-        } catch (error) {
-            console.log(error.response.data)
-        }
+
+  const signUp = async (formData) => {
+    try {
+      console.log(34)
+      const { data } = await axios({
+        url: '/api/auth/signup',
+        method: 'POST',
+        data: { userDetails: formData, bookingDetails }
+      })
+      if (data.status === 'TWO_STEP_AUTHENTICATION') {
+        setIsOtpSent(true)
+        setSessionId(data.sessionId)
+      }
+    } catch (error) {
+      console.log(error.response.data)
     }
-    useEffect(() => {
-        if (query) {
-            // decode query   
-            const decodedString = atob(query);
-            const decodedData = JSON.parse(decodedString);
-            setBookingDetails(decodedData)
-        }
-        else window.location.href = 'http:127.0.0.1:5500'
-    }, [])
- 
+  }
+  useEffect(() => {
+    if (query) {
+      // decode query   
+      const decodedString = atob(query);
+      const decodedData = JSON.parse(decodedString);
+      setBookingDetails(decodedData)
+    }
+    else window.location.href = 'http:127.0.0.1:5500'
+  }, [])
+
 
   return (
     <>
@@ -62,18 +62,13 @@ function Signup() {
         <div className="col-lg-4 col-md-4 col-12 pe-0 mb-5">
           <div className="car-list-sidebar mt-30 h-100">
             <h4 className="title">Booking Form</h4>
-            <form
-              action="#"
-              id="contact-form2"
-              method="POST"
-              className="contact-form-items"
-            >
-              <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 
 
-Where does it come from?
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from </p>
-            </form>
+              Where does it come from?
+              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from </p>
+
           </div>
         </div>
 
@@ -91,138 +86,133 @@ Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots 
                           contact you soon.
                         </p>
                       </div>
-                      {!isOtpSent ? (
-                        <form
-                          onSubmit={handleSubmit(sendOtp)}
-                          className="contact-form-items"
-                        >
-                          <div className="row g-4">
-                            <div className="col-lg-6">
-                              <div className="form-clt">
-                                <label className="label-text">
-                                  First Name *
-                                </label>
-                                <input
-                                  {...register("firstName", {
-                                    required: "First Name is required",
-                                    pattern: namePattern,
-                                  })}
-                                  type="text"
-                                  placeholder="Enter First name"
-                                />
-                                {errors?.firstName?.message && (
-                                  <span>{errors?.firstName?.message}</span>
-                                )}
-                              </div>
-                            </div>
-                            <div className="col-lg-6">
-                              <div className="form-clt">
-                                <label className="label-text">Last Name</label>
-                                <input
-                                  {...register("lastName", {
-                                    required: "Last Name is required",
-                                    pattern: namePattern,
-                                  })}
-                                  type="text"
-                                  placeholder="Enter Last name"
-                                />
-                                {errors?.lastName?.message && (
-                                  <span>{errors?.lastName?.message}</span>
-                                )}
-                              </div>
-                            </div>
-                            <div className="col-lg-6">
-                              <div className="form-clt">
-                                <label className="label-text">Email *</label>
-                                <input
-                                  type="text"
-                                  {...register("email", {
-                                    required: "Email is required",
-                                    pattern: emailPattern,
-                                  })}
-                                  placeholder="Enter Email "
-                                />
-                                {errors?.email?.message && (
-                                  <span>{errors?.email?.message}</span>
-                                )}
-                              </div>
-                            </div>
-                            <div className="col-lg-6">
-                              <div className="form-clt">
-                                <label className="label-text">
-                                  Phone Number
-                                </label>
-                                <div className="position-relative">
-                                  <span className="position-absolute number-code">
-                                    +91
-                                  </span>
-                                  <input
-                                    className="p-60"
-                                    {...register(
-                                      "phoneNumber",
-                                      phoneNumberValidation
-                                    )}
-                                    type="text"
-                                    placeholder="Enter Phone Number"
-                                  />
-                                  {errors?.phoneNumber?.message && (
-                                    <span>{errors?.phoneNumber?.message}</span>
-                                  )}
-                                </div>
-                              </div>
-                            </div>
-                            <div className="col-lg-6">
-                              <div className="form-clt">
-                                <label className="label-text">
-                                  Pick up Address
-                                </label>
-                                <input
-                                  type="text"
-                                  {...register("pickupAddress", {
-                                    required: "Pick up address is required",
-                                  })}
-                                  placeholder="Address"
-                                />
-                                {errors?.pickupAddress?.message && (
-                                  <span>{errors?.pickupAddress?.message}</span>
-                                )}
-                              </div>
-                            </div>
-                            <div className="col-lg-6">
-                              <div className="form-clt">
-                                <label className="label-text">
-                                  Drop Address
-                                </label>
-                                <input
-                                  type="text"
-                                  {...register("dropAddress", {
-                                    required: "Drop address is required",
-                                  })}
-                                  placeholder="Address"
-                                />
-                                {errors?.dropAddress?.message && (
-                                  <span>{errors?.dropAddress?.message}</span>
-                                )}
-                              </div>
-                            </div>
-                            <div className="col-lg-12 d-flex justify-content-end">
-                              <button className="theme-btn" type="submit">
-                                Send Request
-                              </button>
+                      <form
+                        onSubmit={handleSubmit(signUp)}
+                        className="contact-form-items"
+                      >
+                        <div className="row g-4">
+                          <div className="col-lg-6">
+                            <div className="form-clt">
+                              <label className="label-text">
+                                First Name *
+                              </label>
+                              <input
+                                {...register("firstName", {
+                                  required: "First Name is required",
+                                  pattern: namePattern,
+                                })}
+                                type="text"
+                                disabled={isOtpSent}
+                                placeholder="Enter First name"
+                              />
+                              {errors?.firstName?.message && (
+                                <span>{errors?.firstName?.message}</span>
+                              )}
                             </div>
                           </div>
-                        </form>
-                      ) : (
-                        <div>
-                          <input
-                            value={otp}
-                            onChange={(e) => setOtp(e.target.value)}
-                          />
-                          <button onClick={verifyOtp}>verify</button>
+                          <div className="col-lg-6">
+                            <div className="form-clt">
+                              <label className="label-text">Last Name</label>
+                              <input
+                                {...register("lastName", {
+                                  required: "Last Name is required",
+                                  pattern: namePattern,
+                                })}
+                                type="text"
+                                disabled={isOtpSent}
+                                placeholder="Enter Last name"
+                              />
+                              {errors?.lastName?.message && (
+                                <span>{errors?.lastName?.message}</span>
+                              )}
+                            </div>
+                          </div>
+                          <div className="col-lg-6">
+                            <div className="form-clt">
+                              <label className="label-text">Email *</label>
+                              <input
+                                type="text"
+                                {...register("email", {
+                                  required: "Email is required",
+                                  pattern: emailPattern,
+                                })}
+                                disabled={isOtpSent}
+                                placeholder="Enter Email "
+                              />
+                              {errors?.email?.message && (
+                                <span>{errors?.email?.message}</span>
+                              )}
+                            </div>
+                          </div>
+                          <div className="col-lg-6">
+                            <div className="form-clt">
+                              <label className="label-text">
+                                Phone Number
+                              </label>
+                              <div className="position-relative">
+                                <span className="position-absolute number-code">
+                                  +91
+                                </span>
+                                <input
+                                  className="p-60"
+                                  {...register(
+                                    "phoneNumber",
+                                    // phoneNumberValidation
+                                  )}
+                                  type="text"
+                                  disabled={isOtpSent}
+                                  placeholder="Enter Phone Number"
+                                />
+                                {errors?.phoneNumber?.message && (
+                                  <span>{errors?.phoneNumber?.message}</span>
+                                )}
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-lg-6">
+                            <div className="form-clt">
+                              <label className="label-text">
+                                Pick up Address
+                              </label>
+                              <input
+                                type="text"
+                                {...register("pickupAddress", {
+                                  required: "Pick up address is required",
+                                })}
+                                disabled={isOtpSent}
+                                placeholder="Address"
+                              />
+                              {errors?.pickupAddress?.message && (
+                                <span>{errors?.pickupAddress?.message}</span>
+                              )}
+                            </div>
+                          </div>
+                          <div className="col-lg-6">
+                            <div className="form-clt">
+                              <label className="label-text">
+                                Drop Address
+                              </label>
+                              <input
+                                type="text"
+                                {...register("dropAddress", {
+                                  required: "Drop address is required",
+                                })}
+                                disabled={isOtpSent}
+                                placeholder="Address"
+                              />
+                              {errors?.dropAddress?.message && (
+                                <span>{errors?.dropAddress?.message}</span>
+                              )}
+                            </div>
+                          </div>
+                          <div className="col-lg-12 d-flex justify-content-end">
+                            <button className="theme-btn" type="submit">
+                              Send Request
+                            </button>
+                          </div>
                         </div>
-                      )}
-
-                      {!isOtpSent && (
+                      </form>
+                      {isOtpSent && (
                         <div>
                           <input className="cstm-input me-3"
                             value={otp}
