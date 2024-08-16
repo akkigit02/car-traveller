5407605000e623239c0f6bfcccb97036d8a2edaf
Galok Sarkar <galoksarkar@Galoks-Air.lan>
2024-08-16 22:42:05 +0530

    cleanup index file

--- client/script1.js
+++ client/script1.js
@@ -232,7 +232,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const getCities = async (search) => {
         try {
-            let response = await fetch(`http://127.0.0.1:5000/api/client/cities?search=${search}`, {
+            let response = await fetch(`http://127.0.0.1:5001/api/client/cities?search=${search}`, {
                 method: "GET",
             });
             let data = await response.json();
