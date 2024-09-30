9fc49e26d3a51096f39b90eaf2f4ee312ef36b00
cjain <cjain@blinkcharging.com>
2024-09-30 20:04:04 +0530

    login page design

--- client/app/src/components/admin/AdminLogin.js
+++ client/app/src/components/admin/AdminLogin.js
@@ -51,30 +51,92 @@ function AdminLogin() {
             </div>
             <section className="sign-in">
                 <div className="container-login">
+
+
+                    <div className="row m-0">
+                        <div className="col-lg-6 col-md-6 col-12 px-4 py-5 login-card-bg rounded-LTB In-mob">
+                            <div className="my-5 ps-3">
+                                <p className="login-text">Welcome <br></br> Back !</p>
+                                <p>Enjoy affordable rides with the freedom to explore anywhere, anytime—experience limitless journeys with our car rental service!</p>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-6 col-12">
+                            <div className="signin-form p-3">
+                                <div className="w-100 d-flex justify-content-center pt-3">
+                                    <a href="https://dddcabs.com/index.html" rel="noopener noreferrer"><img className="h-60p mb-3" src={logo} alt="logo-img" /></a>
+                                </div>
+                                {otpDetails ? <OtpVerify otpDetails={otpDetails} handleOtpVerify={handleLoginSuccess} /> : <>
+
+
+                                    <form className="register-form" onSubmit={handleSubmit(login)}>
+                                        <div className="cstm-login-input">
+                                            <label>
+                                                Mobile Number
+                                            </label>
+                                            <input
+                                                type="text"
+                                                placeholder="Enter your number"
+                                                {...register("phoneNumber", {
+                                                    required: "Phone Number is required",
+                                                })}
+                                            />
+                                            {errors?.phoneNumber?.message && (
+                                                <span>{errors?.phoneNumber?.message}</span>
+                                            )}
+                                        </div>
+                                        <div className="cstm-login-input">
+                                            <label>
+                                                Password
+                                            </label>
+                                            <input type="password" placeholder="Enter Your password"
+                                                {...register("password", {
+                                                    required: "Password is required",
+                                                    minLength: {
+                                                        value: 6,
+                                                        message: "Min length is 6",
+                                                    },
+                                                })}
+                                            />
+                                            {errors?.password?.message && (
+                                                <span>{errors?.password?.message}</span>
+                                            )}
+                                        </div>
+                                        <div className="form-group-login form-button">
+                                            <button className="form-submit">
+                                                Log in
+                                            </button>
+                                        </div>
+                                    </form>
+                                </>}
+                            </div>
+                        </div>
+
+
+
+                    </div>
+
+
+
+{/* 
                     <div className="signin-content">
-                        {/* <div className="signin-image">
-                            <figure>
-                                <img className="w-100 h-100 mt-3" src={vImg} alt="logo-img" />
-                            </figure>
-                        </div> */}
 
                         <div className="signin-form">
                             <div className="w-100 d-flex justify-content-center">
                                 <a href="https://dddcabs.com/index.html" rel="noopener noreferrer"><img className="h-60p mb-3" src={logo} alt="logo-img" /></a>
                             </div>
                             {otpDetails ? <OtpVerify otpDetails={otpDetails} handleOtpVerify={handleLoginSuccess} /> : <>
-                            <h3 className="mb-4">
+                                <h3 className="mb-4">
                                     Login
                                 </h3 >
                                 <p className="mb-3">
-                                Enjoy affordable rides with the freedom to explore <br></br> anywhere, anytime—experience limitless journeys<br></br> with our car rental service!
-                                </p> 
+                                    Enjoy affordable rides with the freedom to explore <br></br> anywhere, anytime—experience limitless journeys<br></br> with our car rental service!
+                                </p>
                                 <form className="register-form" onSubmit={handleSubmit(login)}>
-                                    <div className="form-group-login">
-                                        <label for="your_name">
-                                            <i className="fa fa-user"></i>
+                                    <div className="cstm-login-input">
+                                        <label>
+                                            Email
                                         </label>
-                                        <input type="text" placeholder="Email"
+                                        <input type="text" placeholder="Enter your email"
                                             {...register("userName", {
                                                 required: "Email is required",
                                                 pattern: emailPattern,
@@ -112,7 +174,7 @@ function AdminLogin() {
                                 </form>
                             </>}
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </section >
         </>
