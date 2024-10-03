06d3ab3551e4ae2adc1c618e70db7022cd70f57b
cjain <cjain@blinkcharging.com>
2024-10-03 22:32:32 +0530

    create FAQ

--- client/script1.js
+++ client/script1.js
@@ -505,3 +505,18 @@ document.addEventListener('DOMContentLoaded', () => {
     selectInputType('cityCab');
 });
 
+const items = document.querySelectorAll('.accordion1 button');
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
