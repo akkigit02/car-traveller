82eb2e770747a0b00ff66116cca6528355b3017a
Avinash Singh <avinash.singh@agamitechnologies.in>
2024-10-03 20:11:14 +0530

    city list fixes

--- server/scripts/city.script.js
+++ server/scripts/city.script.js
@@ -5,12 +5,14 @@ const cities = require('../constants/provideCity.constants');
 const CityModel = require('../models/cities.model');
 
 (async () => {
+    console.log(`Script start...`);
     for (const city of cities) {
         try {
-            await CityModel.updateOne({ name: city }, { isActive: true, isMetroCity: city.isMetrocity });
-            console.log(`Inserted ${bulk.length} cities`);
+            await CityModel.updateOne({ name: city?.city_name }, {$set: { isActive: true, isMetroCity: city.isMetrocity }});
         } catch (err) {
             console.error('Error inserting bulk:', err);
         }
     }
+    console.log('script end...')
+    process.exit()
 })()
